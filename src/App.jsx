206e38841e1400b2942cedbbb3fd0e4cf0d4de02
Praveen Ajjarapu <you@example.com>
2023-12-29 16:37:33 +0530

    path change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import ProductDetails from "./components/ProductDetails";
 import Cart from "./pages/Cart";
